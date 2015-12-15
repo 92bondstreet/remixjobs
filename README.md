850a40d9e32a4ed1df8d31be313194ae4fef3c7d
esilv-s9-ap <esilv-s9-ap@yopmail.com>
2015-12-15 16:07:25 +0100

    TP 4

--- README.md
+++ README.md
@@ -1,93 +1,85 @@
-# Remixjobs
+﻿# RemixJobs_API
 
-> Unofficial Remixjobs API
+> Our (un)official Remixjobs API
 
-## Introduction
+## Get Started
 
-[RemixJobs](https://remixjobs.com/) is the best French job board for the Web Industry.
+Our API allows you to add/search/update/delete jobs from your web application to our database.
 
-Today, no (un)official API was developed to allow developers to add jobs in their web application
+The initial dataSet is taken from RemixJobs.com and is quite exhaustive in terms of available fields.
 
-## Workshop in 1 sentence
+Here is the basic "infos" object returned with the results of every request.
 
-*Build a remixjobs RESTful API*
+"infos": {
+    "version": "1.0",
+    "countResults": 123,
+    "limitResults": 10,
+	"fields":["_id","jobId","title","company","localization","category","description","contract","date","salary","lat","lng","tags"]
+}, 
+"results" : [...]
 
-## How to do that?
+The version, the count of the results and the maximum number of results returned are given in "infos".
 
-Transcript the API description in code ;)
+When you run the app.js you can pass in first parameter the port you wan to use (default is 3000) and/or in first/additional parameter if you want to "drop"/"load" the table. If you pass "drop", "load" is optional because the table will automatically be reloaded.
+Example : "node app.js 8080 drop" will drop and reload the table, then the API will listen on port 8080.
 
-The goal is write enough endpoints with the right filters and fields to build a specific job boards as:
+## Usage
 
-* A job board for JavaScript intership
-* A job board for remote jobs on Big Data
-* A job board for Designers who want sunny weather... so in South Of France
+### /home
 
-Etc, etc, etc...
-
-## Stack
-
-* Node.js
-* Express 4
-* MongoDB
-* Postman
-
-## Api description
-
-We could define a job by its
-
-1. Job title
-1. Company
-1. Localization
-1. Category
-1. Description
-1. Contract
-1. Date
-1. Tags
+Whenever you use a malformed path or an endpoint which is not available, you will be automatically redirected to the endpoint "home".
+Example : http://localhost:3000/api/v1.0/home
+Moreover, you will get a dynamic presentation message that introduces the API.
+Example : WELCOME TO REMIXJOBS API ! Please use the route api/v1.0/ and one of the available endpoints : home,jobs,latest,today,distinct.
 
 ### /jobs
 
-* Return all jobs
-* Create a new job
-* Return information of a job
-* Update a jobs
+Is the main endpoint of the API. Here are all the jobs. You can access them precisely with query parameters.
 
-#### Non exhaustive parameters
+Example : GET http://localhost:3000/api/v1.0/jobs will return all the jobs.
+Example : GET http://localhost:3000/api/v1.0/jobs?localization=Paris&tags=php will return all the jobs corresponding to the request parameters.
 
-parameters | description
----------- | -----------
-q | the query
-contract | filter by contract (cdi, cdd...)
-category | design, dev...
-where | localization
-limit | Jobs number
+You can also POST, PUT, DELETE passing parameters in the URL and the JSON object in the body of the request.
 
-### /jobs/latest
+### /today
 
-* Return all jobs of the current day
+Returns all the jobs uploaded the same day of the request.
 
-### /companies
+Example : GET http://localhost:3000/api/v1.0/today
 
-* Return all companies
-* Return all jobs of a the given companies
+### /latest
 
-## Jobs model
+Returns the very latest jobs (very last time of addition)
 
-I think that the first step is to scrap datas from RemixJobs website and fill a [mongoDB](https://www.mongodb.org/) database.
+Example : GET http://localhost:3000/api/v1.0/jobs
 
-Once database filled, your api will fetch/save/update data from this database in a real API consuming.
+### /distinct
 
+Here you can check the distinct elements of every attributes.
 
-## API rules (at least to follow)
+Example : http://localhost:3000/api/v1.0/distinct?attribute=category 
+Example : http://localhost:3000/api/v1.0/distinct?attribute=localization 
 
-1. RESTful URLs and actions
-1. Plural noun
-1. Version via the url
-1. Query parameters for advanced filtering, sorting and searching
-1. Limit fields are returned from the APO
-1. JSON only
-1. snake_case for resources
-1. Use HTTP Status codes
+### Logs
 
-## Licence
+During all the lifecycle of the server, your console logs precisely what occurs in the database and all the requests.
 
-[Uncopyrighted](http://zenhabits.net/uncopyright/)
+Example :
+Debugger listening on port 5858
+API running on port 3000
+Connected to MongoDB server
+936 jobs are currently stored.
+GET /
+GET /api/v1.0/home
+GET /api/v1.0/today
+GET /api/v1.0/latest
+POST /api/v1.0/jobs
+PUT /api/v1.0/jobs?company=ESILV
+DELETE /api/v1.0/jobs?localization=La%20D%C3%A9fense
+GET /api/v1.0/jobs?localization=Paris&tags=php
+
+## Conclusion
+
+Enjoy our API !
+
+Thank You !
---
+++ app.js
@@ -0,0 +1,249 @@
+﻿/* RemixJobs API */
+
+//REQUIREMENTS
+var mongoClient = require("mongodb").MongoClient;
+var http = require("http");
+var url = require("url");
+var querystring = require("querystring");
+var assert = require("assert");
+var fs = require("fs");
+
+var dbUrl = "mongodb://localhost:27017/remixjobs";
+var name = "api";
+var versionNumber = "1.0";
+var version = "v1.0";
+var nbMaxResults = 10;
+var port = isNaN(parseInt(process.argv[2]))?3000:parseInt(process.argv[2]); // PORT IS FIRST GIVEN process.argv OR 3000
+var collection;
+var sample;
+
+// MONGO CLIENT
+mongoClient.connect(dbUrl, function (err, db) {
+    assert.equal(err, null);
+    console.log("Connected to MongoDB server");
+    collection = db.collection('jobs');
+    collection.count(function (err1, count) {
+        assert.equal(err1, null);
+        // (RE)LOAD THE DATASET INTO MONGODB IF "load" IS IN process.argv OR IF collection.count === 0
+        if (count === 0 || ~process.argv.slice(2).indexOf("load")) {
+            LoadDataSet();
+        }
+        // DROP DB AND LOAD THE DATASET INTO MONGODB IF "drop" IS IN process.argv
+        else if (~process.argv.slice(2).indexOf("drop")) {
+            collection.drop(function () {
+                console.log(count + " jobs were dropped.");
+                LoadDataSet();
+            });
+        }
+        // ELSE, SHOW DB COUNT
+        else {
+            console.log(count + " jobs are currently stored.");
+            LoadSample();
+        }
+    });
+});
+
+// LOAD JSON DATASET
+function LoadDataSet(callback) {
+    var rawDataSet = fs.readFileSync('remixjobs.json').toString();
+    var dataSet = JSON.parse(rawDataSet.substring(rawDataSet.search(/[\[\{a-z0-9]/i)));
+    var dataSetCount = dataSet.length;
+    collection.insertMany(dataSet, function (err) {
+        assert.equal(err, null);
+        console.log(dataSetCount + " jobs were successfully loaded.");
+        collection.count(function (err1, count) {
+            assert.equal(err1, null);
+            console.log(count + " jobs are currently stored.");
+            LoadSample();
+            if (callback) {
+                callback(count);
+            }
+        });
+    });
+}
+
+// LOAD SAMPLE
+function LoadSample() {
+    collection.findOne({}, function (err, doc) {
+        assert.equal(err, null);
+        sample = doc;
+    });
+}
+
+// PROTOTYPING HTTP
+http.ServerResponse.prototype.Redirect = function (redirectionUrl) {
+    try { this.writeHead(302, { "Location": redirectionUrl }); }
+    catch (e) { this.writeHead(404, { "Content-Type": "text/html", "charset" : "UTF-8" }); }
+    finally { this.end(); }
+}
+
+http.ServerResponse.prototype.SendError = function () {
+    this.writeHead(404, { "Content-Type": "text/html", "charset" : "UTF-8" });
+    this.end();
+}
+
+http.ServerResponse.prototype.SendJSON = function (obj, statusCode) {
+    statusCode = statusCode || 200;
+    this.writeHead(statusCode, { "Content-Type": "application/json", "charset" : "UTF-8" });
+    this.write(JSON.stringify(obj));
+    this.end();
+}
+
+http.ServerResponse.prototype.Send = function (str, statusCode, contentType) {
+    statusCode = statusCode || 200;
+    this.writeHead(statusCode, { "Content-Type": contentType, "charset" : "UTF-8" });
+    this.write(str);
+    this.end();
+}
+
+http.ServerResponse.prototype.SendFromAPI = function (result, statusCode, contentType) {
+    statusCode = statusCode || 200;
+    contentType = contentType || "application/json";
+    result = result.constructor.name === "Array" ? result : [result];
+    var objResponse = { infos : { version : versionNumber, countResults : result.length, limitResults : nbMaxResults, fields : Object.keys(sample) }, results : result.slice(0, nbMaxResults) };
+    this.writeHead(statusCode, { "Content-Type": contentType, "charset" : "UTF-8" });
+    this.write(JSON.stringify(objResponse));
+    this.end();
+}
+
+http.IncomingMessage.prototype.GetBody = function (callback) {
+    var req = this;
+    var body = "";
+    req.on('data', function (chunk) {
+        body += chunk.toString();
+    });
+    req.on('end', function () {
+        callback(body);
+    });
+}
+
+// SERVER
+var server = http.createServer(function (req, res) {
+    var method = req.method;
+    var pathname = url.parse(req.url).pathname;
+    var query = url.parse(req.url).query;
+    var href = url.parse(req.url).href;
+    var dir = pathname.substring(0, pathname.lastIndexOf("/")).replace("/", "");
+    var file = pathname.substring(pathname.lastIndexOf("/")).replace("/", "");
+    var params = querystring.parse(query);
+
+    console.log(method + " " + href);
+    
+    var args = [req, dir, file, params, method];
+
+    var route = findRoute.apply(routes, args);
+
+    if (route) {
+        route.apply(res, args);
+    }
+    else {
+        res.Redirect(home);
+    }
+});
+server.listen(port);
+console.log("API running on port " + port);
+
+// ROUTES
+var place = name + "/" + version + "/";
+var home = "/" + name + "/" + version + "/home";
+var routes = {}
+routes[name] = {};
+var endPoints = routes[name][version] = {};
+
+endPoints['home'] = {
+    GET : function () {
+        var res = this;
+        res.write("WELCOME TO REMIXJOBS API\nPlease use the route " + place + " and one of the available endpoints : " + Object.keys(endPoints));
+        res.end();
+    }
+};
+
+endPoints['jobs']= {
+    POST : function (req) {
+        var res = this;
+        req.GetBody(function (body) {
+            var obj = JSON.parse(body);
+            collection.insertOne(obj, function (err, result) {
+                assert.equal(err, null);
+                res.SendFromAPI(result);
+            });
+        });
+    },
+    GET : function (req, dir, file, params) {
+        var res = this;
+        collection.find(params).toArray(function (err, docs) {
+            assert.equal(err, null);
+            res.SendFromAPI(docs);
+        });
+    },
+    PUT : function (req, dir, file, params) {
+        var res = this;
+        req.GetBody(function (body) {
+            var obj = JSON.parse(body);
+            collection.updateOne(params, { $set: obj }, function (err, result) {
+                assert.equal(err, null);
+                res.SendFromAPI(result);
+            });
+        });
+    },
+    DELETE : function (req, dir, file, params) {
+        var res = this;
+        collection.deleteOne(params, function (err, result) {
+            assert.equal(err, null);
+            res.SendFromAPI(result);
+        });
+    }
+};
+
+endPoints['latest'] = {
+    GET : function (req, dir, file, params) {
+        var res = this;
+        collection.find(params, { "sort": [['jobId', 'desc'], ['title', 'asc']] }).toArray(function (err, docs) {
+            assert.equal(err, null);
+            if (docs[0]) {
+                params['date'] = docs[0]['date'];
+                collection.find(params).toArray(function (err1, docs) {
+                    assert.equal(err1, null);
+                    res.SendFromAPI(docs);
+                });
+            } else {
+                res.SendFromAPI([]);
+            }
+        });
+    }
+};
+
+endPoints['today'] = {
+    GET : function ()  {
+        var res = this;
+        collection.find({date : {$regex : ".*heure.*"}}).toArray(function (err, docs) {
+            assert.equal(err, null);
+            res.SendFromAPI(docs);
+        });
+    }
+};
+
+endPoints['distinct'] = {
+    GET : function (req, dir, file, params) {
+        var res = this;
+        if (params['attribute']) {
+            collection.distinct(params['attribute'], function(err, items) {
+                assert.equal(err, null);
+                res.SendFromAPI(items);
+            });
+        } else {
+            res.SendFromAPI([]);
+        }
+    }
+};
+
+// ROUTING
+function findRoute(req, dir, file, params, method) {
+    var route = this;
+    var path = dir + "/" + file + "/" + method;
+    var points = path.split("/");
+    while (points.length > 0 && route) {
+        route = route[points.shift()];
+    }
+    return route;
+}
